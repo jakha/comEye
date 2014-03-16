18cb6f8bbd8fa7bc3e56ce2775c4bf31e45664cc
jakha <jaha@ereshov.ru>
2014-03-16 15:17:51 +0400

    all

---
+++ CSTrackaccess/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace CSTrackaccess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ibOriginal = new Emgu.CV.UI.ImageBox();
+            this.btnPauseOrResume = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ibOriginal
+            // 
+            this.ibOriginal.Location = new System.Drawing.Point(13, 13);
+            this.ibOriginal.Margin = new System.Windows.Forms.Padding(4);
+            this.ibOriginal.Name = "ibOriginal";
+            this.ibOriginal.Size = new System.Drawing.Size(623, 490);
+            this.ibOriginal.TabIndex = 2;
+            this.ibOriginal.TabStop = false;
+            // 
+            // btnPauseOrResume
+            // 
+            this.btnPauseOrResume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPauseOrResume.Location = new System.Drawing.Point(13, 511);
+            this.btnPauseOrResume.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPauseOrResume.Name = "btnPauseOrResume";
+            this.btnPauseOrResume.Size = new System.Drawing.Size(151, 53);
+            this.btnPauseOrResume.TabIndex = 4;
+            this.btnPauseOrResume.Text = "pause";
+            this.btnPauseOrResume.UseVisualStyleBackColor = true;
+            this.btnPauseOrResume.Click += new System.EventHandler(this.btnPauseOrResume_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 582);
+            this.Controls.Add(this.btnPauseOrResume);
+            this.Controls.Add(this.ibOriginal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kinect";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox ibOriginal;
+        private System.Windows.Forms.Button btnPauseOrResume;
+    }
+}
+
---
+++ CSTrackaccess/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+namespace CSTrackaccess
+{
+    public partial class Form1 : Form
+    {
+        Capture capWebCam = null;
+        private HaarCascade haarCasc;
+        bool blnCapturingProcess = false;
+        Image<Bgr, Byte> imgOriginal;
+        float scaleH, scaleW;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                capWebCam = new Capture();
+            }
+            catch (NullReferenceException except)
+            {         
+                return;
+            }
+            haarCasc = new HaarCascade("haarcascade_frontalface_alt2.xml");
+            Application.Idle += processFrameAndUpdateGUI;
+            blnCapturingProcess = true;
+            Cursor.Position = new Point(SystemInformation.PrimaryMonitorSize.Width / 2, SystemInformation.PrimaryMonitorSize.Height / 2);
+            scaleW = SystemInformation.PrimaryMonitorSize.Width / ibOriginal.Size.Width;
+            scaleH = SystemInformation.PrimaryMonitorSize.Height / ibOriginal.Size.Height;
+
+        }
+
+        private void Form1_FormClosed( object sender, FormClosedEventArgs e)
+        {
+            if (capWebCam != null)
+            {
+                capWebCam.Dispose();
+            }
+        }
+
+        void processFrameAndUpdateGUI(object sender, EventArgs arg)
+        {
+           /* imgOriginal = capWebCam.QueryFrame();
+            if (imgOriginal == null) return;
+
+            imgProcess = imgOriginal.InRange(new Bgr(0,0,175), new Bgr(100,100,256));
+            imgProcess = imgProcess.SmoothGaussian(9);
+            CircleF[] circles = imgProcess.HoughCircles(new Gray(100), new Gray(50), 2,
+                                                            imgProcess.Height/4,10,400)[0];
+            foreach (CircleF circle in circles)
+            {
+                if (txtXYRadius.Text != "") txtXYRadius.AppendText( Environment.NewLine);
+                txtXYRadius.AppendText("ball position = x " +circle.Center.X.ToString().PadLeft(4) 
+                                            + ", y =  " + circle.Center.X.ToString().PadLeft(4) +
+                                            ", radius = " +circle.Radius.ToString("###.000").PadLeft(7));
+                txtXYRadius.ScrollToCaret();
+
+                CvInvoke.cvCircle(imgOriginal, new Point ((int) circle.Center.X, (int) circle.Center.Y), 3, new MCvScalar(0,255,0), -1, LINE_TYPE.CV_AA, 0);
+                imgOriginal.Draw(circle, new Bgr(Color.Red), 3);                
+            }
+            */
+          
+
+            using (Image<Bgr, Byte> nextFrame = capWebCam.QueryFrame())
+            {
+                if (nextFrame != null)
+                {
+                    Image<Gray, byte> grayFrame = nextFrame.Convert<Gray, byte>();
+                    var faces = grayFrame.DetectHaarCascade(haarCasc,1.4,4, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                                                                    new Size(nextFrame.Width/8, nextFrame.Height/8))[0];
+                    foreach (var face in faces)
+                    {
+                        nextFrame.Draw(face.rect, new Bgr(7, double.MaxValue, 0), 3);
+                        Cursor.Position = new Point((int)(face.rect.X * scaleW + face.rect.Width/2), 
+                            (int) (face.rect.Y *scaleH + face.rect.Height/2));                       
+                    }
+
+                    ibOriginal.Image = nextFrame;
+                }
+            }
+        }
+
+        private void btnPauseOrResume_Click(object sender, EventArgs e)
+        {
+            if (blnCapturingProcess == true)
+            {
+                Application.Idle -= processFrameAndUpdateGUI;
+                blnCapturingProcess = false;
+                btnPauseOrResume.Text = "resume";
+            }
+            else
+            {
+                Application.Idle += processFrameAndUpdateGUI;
+                blnCapturingProcess = true;
+                btnPauseOrResume.Text = "pause";
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
